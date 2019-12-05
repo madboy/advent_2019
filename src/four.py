e492269223ff12bb5ce9ff8bcc5ad2b4e1044dc7
Kristofer Lindberg <k.lindberg@gmail.com>
2019-12-05 07:56:31 +0100

    slight tweak on day 4

--- src/four.py
+++ src/four.py
@@ -4,19 +4,18 @@ from src.tools import process
 def criteria(number):
     n = list(str(number))
     if len(n) == len(set(n)):
-        return False
+        return False, False
     if n == sorted(n):
-        return True
-    return False
+        return True, criteria2(n)
+    return False, False
 
 
 def count(l, x):
     return l.count(x)
 
 
-def criteria2(number):
+def criteria2(n):
     # assumes we have checked critera first
-    n = list(str(number))
     match2 = (count(n, x) == 2 for x in n)
     return any(match2)
 
@@ -27,10 +26,10 @@ def run(input_file):
     for line in process(input_file):
         low, high = [int(n) for n in line.split("-")]
         for number in range(low, high):
-            if criteria(number):
+            pass1, pass2 = criteria(number)
+            if pass1:
                 part1_count += 1
-                # second criteria is subset of first
-                if criteria2(number):
-                    part2_count += 1
+            if pass2:
+                part2_count += 1
 
     print(part1_count, part2_count)
