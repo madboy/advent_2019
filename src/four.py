e8e285a4e5210a0143584c09835a111dd3f6dcbe
Kristofer Lindberg <k.lindberg@gmail.com>
2019-12-06 17:36:12 +0100

    add day 3 and 4 tests

--- src/four.py
+++ src/four.py
@@ -1,3 +1,4 @@
+import pytest
 from src.tools import process
 
 
@@ -20,16 +21,43 @@ def criteria2(n):
     return any(match2)
 
 
-def run(input_file):
+def solve_part1_and_part2(low, high):
     part1_count = 0
     part2_count = 0
+    for number in range(low, high):
+        pass1, pass2 = criteria(number)
+        if pass1:
+            part1_count += 1
+        if pass2:
+            part2_count += 1
+    return part1_count, part2_count
+
+
+def run(input_file):
     for line in process(input_file):
         low, high = [int(n) for n in line.split("-")]
-        for number in range(low, high):
-            pass1, pass2 = criteria(number)
-            if pass1:
-                part1_count += 1
-            if pass2:
-                part2_count += 1
+        part1, part2 = solve_part1_and_part2(low, high)
+        print(part1)
+        print(part2)
 
-    print(part1_count, part2_count)
+
+def test_part1_and_part2():
+    part1, part2 = solve_part1_and_part2(109165, 576723)
+    assert part1 == 2814
+    assert part2 == 1991
+
+
+@pytest.mark.parametrize(
+    "number, expected",
+    [
+        pytest.param(111111, (True, False)),
+        pytest.param(223450, (False, False)),
+        pytest.param(123789, (False, False)),
+        pytest.param(112233, (True, True)),
+        pytest.param(123444, (True, False)),
+        pytest.param(111122, (True, True)),
+    ],
+)
+def test_criteria(number, expected):
+    part1, part2 = criteria(number)
+    assert (part1, part2) == expected
--- src/three.py
+++ src/three.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import pytest
 from src.tools import process
 
 
@@ -14,11 +15,20 @@ class Point:
         return self.x == other.x and self.y == other.y
 
 
-def run(input_file):
+def get_wires(input_file):
     wires = []
     for line in process(input_file):
         wires.append([m for m in line.split(",")])
+    return wires
 
+
+def run(input_file):
+    part1, part2 = solve_part1_and_part2(get_wires(input_file))
+    print(part1)
+    print(part2)
+
+
+def solve_part1_and_part2(wires):
     places = set()
     current_manhattan = 10000
     current_steps = 10000
@@ -51,4 +61,47 @@ def run(input_file):
                     csteps[new] = wire_steps
 
             start = new
-    print(current_manhattan, current_steps)
+    return (current_manhattan, current_steps)
+
+
+@pytest.mark.parametrize(
+    "wires,expected",
+    [
+        pytest.param(
+            [
+                ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"],
+                ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"],
+            ],
+            (159, 610),
+        ),
+        pytest.param(
+            [
+                [
+                    "R98",
+                    "U47",
+                    "R26",
+                    "D63",
+                    "R33",
+                    "U87",
+                    "L62",
+                    "D20",
+                    "R33",
+                    "U53",
+                    "R51",
+                ],
+                ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"],
+            ],
+            (135, 410),
+        ),
+    ],
+)
+def test_part1_and_part2_examples(wires, expected):
+    response = solve_part1_and_part2(wires)
+    assert response == expected
+
+
+def test_part1_and_part2():
+    wires = get_wires("input/3")
+    part1, part2 = solve_part1_and_part2(wires)
+    assert part1 == 860
+    assert part2 == 9238
