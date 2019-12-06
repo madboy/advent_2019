72fe422783656a0935377755b98fd92b47826fb0
Kristofer Lindberg <k.lindberg@gmail.com>
2019-12-06 11:34:32 +0100

    only go to COM

--- src/six.py
+++ src/six.py
@@ -22,13 +22,12 @@ def run(input_file):
 def solve_part1(graph):
     total = 0
     for so in graph:
-        # don't count the end node
-        total += len(find_path(so, None, graph)) - 1
+        total += len(find_path(so, "COM", graph))
     print(total)
 
 
 def solve_part2(graph):
-    you = set(find_path("YOU", None, graph))
-    san = set(find_path("SAN", None, graph))
+    you = set(find_path("YOU", "COM", graph))
+    san = set(find_path("SAN", "COM", graph))
     # if we remove common nodes we should have number of jumps between us
     print(len(you) + len(san) - 2 * len(you.intersection(san)))
