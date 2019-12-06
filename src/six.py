63027fda61d4d0d3bd7c050e5a3d2f55cd27c589
Kristofer Lindberg <k.lindberg@gmail.com>
2019-12-06 11:31:04 +0100

    break out path finding

--- src/six.py
+++ src/six.py
@@ -1,6 +1,14 @@
 from src.tools import process
 
 
+def find_path(start, end, graph):
+    path = []
+    while start != end:
+        start = graph[start]
+        path.append(start)
+    return path
+
+
 def run(input_file):
     graph = {"COM": None}
     for line in process(input_file):
@@ -14,22 +22,13 @@ def run(input_file):
 def solve_part1(graph):
     total = 0
     for so in graph:
-        while so != None:
-            so = graph[so]
-            # we only want to count real nodes
-            # and not the end node None
-            total += 1 if so else 0
+        # don't count the end node
+        total += len(find_path(so, None, graph)) - 1
     print(total)
 
 
 def solve_part2(graph):
-    paths = []
-    for so in ["YOU", "SAN"]:
-        path = set()
-        while so != None:
-            so = graph[so]
-            path.add(so)
-        paths.append(path)
-    you = paths[0]
-    san = paths[1]
+    you = set(find_path("YOU", None, graph))
+    san = set(find_path("SAN", None, graph))
+    # if we remove common nodes we should have number of jumps between us
     print(len(you) + len(san) - 2 * len(you.intersection(san)))
