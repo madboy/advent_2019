9dc01f728093179c7da966b480dad5a85418845a
Kristofer Lindberg <k.lindberg@gmail.com>
2019-12-06 21:37:21 +0100

    faster without dataclass

--- src/three.py
+++ src/three.py
@@ -1,18 +1,5 @@
-from dataclasses import dataclass
 import pytest
 from tools.tools import process, timing
-
-
-@dataclass
-class Point:
-    x: int
-    y: int
-
-    def __hash__(self):
-        return hash((self.x, self.y))
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
 
 
 def get_wires(input_file):
@@ -35,23 +22,23 @@ def solve_part1_and_part2(wires):
     current_steps = 10000
     csteps = {}
     for w, wire in enumerate(wires):
-        start = Point(0, 0)
+        start = (0, 0)
         wire_steps = 0
         for movement in wire:
             direction, steps = movement[0], int(movement[1:])
             for m in range(1, steps + 1):
                 wire_steps += 1
                 if direction == "R":
-                    new = Point(start.x + m, start.y)
+                    new = (start[0] + m, start[1])
                 elif direction == "L":
-                    new = Point(start.x - m, start.y)
+                    new = (start[0] - m, start[1])
                 elif direction == "U":
-                    new = Point(start.x, start.y + m)
+                    new = (start[0], start[1] + m)
                 elif direction == "D":
-                    new = Point(start.x, start.y - m)
+                    new = (start[0], start[1] - m)
                 # only count it as crossing if we are on the second wire
                 if w == 1 and new in places:
-                    if current_manhattan > (manhattan := abs(new.x) + abs(new.y)):
+                    if current_manhattan > (manhattan := abs(new[0]) + abs(new[1])):
                         current_manhattan = manhattan
                     if (steps_to_here := csteps[new] + wire_steps) < current_steps:
                         current_steps = steps_to_here
