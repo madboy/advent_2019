1c73f8e93c667681a63c72371c03ade13ce18fe5
Kristofer Lindberg <k.lindberg@gmail.com>
2019-12-19 20:13:43 +0100

    update template

--- tools/template.py
+++ tools/template.py
@@ -1,10 +1,28 @@
-from src.tools import process
+from tools.tools import process, timing
 
 
 def run(input_file):
-    for line in process(input_file):
-        print(line)
+    with timing("template"):
+        for line in process(input_file):
+            solve_part1(line)
+            solve_part2(line)
 
 
-def solve(input):
-    pass
+def solve_part1(line):
+    return "None"
+
+
+def solve_part2(line):
+    return "None"
+
+
+def test_part1_example():
+    line = ""
+    response = solve_part1(line)
+    assert response == "None"
+
+
+def test_part2_example():
+    line = ""
+    response = solve_part1(line)
+    assert response == "None"
